b9bcd925f4669fab4a0785075a55ff7c63974e58
Jacques Bellavance <j-bellavance@videotron.ca>
2017-08-25 09:23:04 -0400

    Add files via upload

---
+++ EdgeDebounce/examples/Reading/Reading.ino
@@ -0,0 +1,39 @@
+/*
+ * Reading.ino
+ * By Jacques Bellavance, July 20 2017
+ * Released into the public domain
+ * GNU GENERAL PUBLIC LICENSE V3
+ *
+ * Shows how to use the EdgeDebounce Library
+ * Reading a switch is when you are only interested to know if it changed state.
+ * This sketch will poll the switch and only report when its state has changed.
+*/
+
+#include <EdgeDebounce.h>
+
+#define BUTTON_PIN 2
+
+//Create an instance of Debounce and name it button
+//button is tied to pin BUTTON_PIN and is in PULLUP mode
+EdgeDebounce button(BUTTON_PIN, PULLUP);  
+
+bool newState;
+bool currentState;
+String msg[2] = {"Now open", "Now closed"};
+
+void setup() {
+  button.begin();                         //Setup button
+  currentState  = button.closed();        //Set current state
+  Serial.begin(9600);                     //Set Serial
+  Serial.println(msg[currentState]);      //print current state
+}
+
+void loop() {
+  newState = button.closed();        //Read new state
+  if (newState != currentState) {    //If it changed
+    currentState = newState;           //Record new state
+    Serial.println(msg[newState]);     //Print new state
+  }  
+//Do other stuff
+}
+
---
+++ EdgeDebounce/examples/ShortAndLong/ShortAndLong.ino
@@ -0,0 +1,47 @@
+/*
+ * ShortAndLong.ino
+ * By Jacques Bellavance, July 20 2017
+ * GNU GENERAL PUBLIC LICENSE V3
+ *
+ * Shows how to use the EdgeDebounce Library 
+ * At times, you may want to do different things
+ * if the swich is pressed for a short time or
+ * if it is pressed for a long time.
+ * In order to do this, we have to detect two events:
+ * - The moment that the switch's state changes from open to closed
+ *   At this point we need to reset the chronometer and 
+ * - The moment that the switch's state changes from closed to open
+ *   At this point, we can determine how long the switch has been pressed
+ *   and take the appropriate action. (Printing "Short" or "Long")
+*/
+
+#include <EdgeDebounce.h>
+
+#define BUTTON_PIN 4
+
+//Create an instance of Debounce and name it button
+//button is tied to pin BUTTON_PIN and is in PULLUP mode
+EdgeDebounce button(BUTTON_PIN, PULLUP);  
+
+unsigned long chrono;         //Set chronometer
+unsigned long limit = 500;   //milliseconds (2 seconds)
+
+void setup() {
+  button.begin();
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  button.update();
+  if (button.rose())  {                //Just closed?
+    chrono = millis();                   //Reset Chrono
+  }  
+  if (button.fell()) {                 //Just opened?
+    if (millis() - chrono < limit)       //If lower than limit
+      Serial.println(F("Short"));          //print "Short"
+    else                                 //Otherwise
+      Serial.println(F("Long"));           //print "Long"
+  }
+  //Do other stuff
+}
