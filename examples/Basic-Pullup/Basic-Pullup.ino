e234873f8276479510656bba1d41d5c4d82e0892
Jacques Bellavance <j-bellavance@videotron.ca>
2017-08-25 09:24:37 -0400

    Add files via upload

---
+++ examples/Basic-Pullup/Basic-Pullup.ino
@@ -0,0 +1,30 @@
+/*
+ * Basic.ino
+ * By Jacques Bellavance, July 7 2017
+ * Released into the public domain
+ * GNU GENERAL PUBLIC LICENSE V3
+ *
+ * Shows how to use the EdgeDebounce Library
+ * This sketch is for switches that uses the internal pullup resistor
+ * Switch pin A : Ground
+ * Switch pin B : Arduino's pin 2
+*/
+
+#include <EdgeDebounce.h>
+
+#define BUTTON_PIN 2
+
+//Create an instance of Debounce and name it button
+//button is tied to pin BUTTON_PIN and is in PULLUP mode
+EdgeDebounce button(BUTTON_PIN, PULLUP);  
+
+void setup() {
+  button.begin();       //Initialise pinMode() for button
+  pinMode(13, OUTPUT);  //Initialise the LED pin
+}
+
+void loop() {
+  if (button.closed()) digitalWrite(13, HIGH);  //The .closed method returns true if there is contact
+  else                 digitalWrite(13, LOW);
+}
+
---
+++ examples/Toggle/Toggle.ino
@@ -0,0 +1,37 @@
+/*
+ * Toggle.ino
+ * By Jacques Bellavance, July 20 2017
+ * GNU GENERAL PUBLIC LICENSE V3
+ *
+ * Shows how to use the EdgeDebounce Library 
+ * Transform a momentary button switch into an on/off switch
+ * Pressing (and releasing the button) makes the switchState variable
+ * go from on to off and vice versa.
+*/
+
+#include <EdgeDebounce.h>
+
+#define BUTTON_PIN 4
+#define LED_PIN 13
+
+//Create an instance of Debounce and name it button
+//button is tied to pin BUTTON_PIN and is in PULLUP mode
+EdgeDebounce button(BUTTON_PIN, PULLUP);  
+
+bool switchState;
+
+void setup() {
+  button.begin();                   //Set up button
+  pinMode(LED_PIN, OUTPUT);         //Set up LED pin
+  switchState = button.closed();    //Set up state to current pin state
+}
+
+void loop() {
+  button.update();
+  if (button.fell()) {            //If the button clicked (rose and then fell)
+    switchState = !switchState;   //Toggle state
+  }
+  digitalWrite(LED_PIN, switchState);
+  //Do other stuff
+}
+
